258ff907c1508c51868d4dbab741298e94873ac8
Thiago Nogueira Coelho <th.nogueira21@gmail.com>
2024-01-18 23:23:45 -0300

    Jwt Token Provider

--- 13_ContentNegotiation/rest-com-springboot-e-java/src/main/java/br/com/spring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 13_ContentNegotiation/rest-com-springboot-e-java/src/main/java/br/com/spring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.spring.exceptions.ExceptionResponse;
+import br.com.spring.exceptions.InvalidJwtAuthenticationException;
 import br.com.spring.exceptions.RequiredObjectIsNullException;
 import br.com.spring.exceptions.ResourceNotFoundException;
 
@@ -46,5 +47,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				ex.getMessage(),
 				request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(
+			Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 }
---
+++ 13_ContentNegotiation/rest-com-springboot-e-java/src/main/java/br/com/spring/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package br.com.spring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.spring.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Query("SELECT u FROM User WHERE u.userName = :userName")
+	User findByUsername(@Param("userName") String userName);
+}
