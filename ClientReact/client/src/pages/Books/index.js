11372567db428e70c540ed894f0977b2a235c494
Thiago Nogueira Coelho <th.nogueira21@gmail.com>
2024-04-05 22:27:02 -0300

    Listing books on React client

--- ClientReact/client/src/pages/Books/index.js
+++ ClientReact/client/src/pages/Books/index.js
@@ -1,17 +1,42 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link,  } from 'react-router-dom';
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImage from '../../assets/logo.svg';
 
 export default function Books() {
+
+    const [books, setBooks] = useState([]);
+
+    const username = localStorage.getItem('username');
+    const accessToken = localStorage.getItem('accessToken');
+
+    // const navigate = useNavigate();
+
+
+    useEffect(() => {
+        api.get('/api/book/v1', { 
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            params: {
+                limit: 4
+            }
+        } 
+        ).then(response => {
+           setBooks(response.data._embedded.bookVOList);
+        });
+    });
+
     return (
         <div className="book-container">
             <header>
                 <img src={logoImage} alt="Erudio"/>
-                <span>Bem-vindo, <strong>Usuário</strong>!</span>
+                <span>Bem-vindo, <strong>{ username.toUpperCase() }</strong>!</span>
                 <Link className="button" to="/book/new">Adicionar novo livro</Link>
                 <button type="button">
                     <FiPower size={18} color="#251fc5" />
@@ -20,96 +45,26 @@ export default function Books() {
 
             <h1>Livros cadastrados</h1>
             <ul>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>Irineu</p>
-                    <strong>Preço:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Data de lançamento:</strong>
-                    <p>12/07/2017</p>
+                {books.map(book =>
+                    <li>
+                        <strong>Título:</strong>
+                        <p>{book.title}</p>
+                        <strong>Autor:</strong>
+                        <p>{book.author}</p>
+                        <strong>Preço:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(book.price)}</p>
+                        <strong>Data de lançamento:</strong>
+                        <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
 
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
+                        <button type="button">
+                            <FiEdit size={20} color="#251fc5"/>
+                        </button>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>Irineu</p>
-                    <strong>Preço:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Data de lançamento:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>Irineu</p>
-                    <strong>Preço:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Data de lançamento:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>Irineu</p>
-                    <strong>Preço:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Data de lançamento:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Título:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>Irineu</p>
-                    <strong>Preço:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Data de lançamento:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
+                        <button type="button">
+                            <FiTrash2 size={20} color="#251fc5"/>
+                        </button>
+                    </li>
+                )}
             </ul>
         </div>
     )
--- ClientReact/client/src/pages/NewBook/index.js
+++ ClientReact/client/src/pages/NewBook/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate  } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImage from '../../assets/logo.svg';
 
 export default function NewBook() {
+
+    
+    // const [id, setId] = useState(null);
+    const [author, setAuthor] = useState('');
+    const [launchDate, setLaunchDate] = useState('');
+    const [price, setPrice] = useState('');
+    const [title, setTitle] = useState('');
+
+    // const username = localStorage.getItem('username');
+    const accessToken = localStorage.getItem('accessToken');
+
+    const navigate = useNavigate();
+
+    async function createNewBook(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            author,
+            launchDate,
+            price
+        }
+
+        const headers = {
+            Authorization: `Bearer ${accessToken}`
+        }
+
+        try {
+            await api.post('/api/book/v1', data, { headers });
+            navigate('/books');
+        } catch(err) {
+            alert('Erro ao salvar livro! Tente novamente!');
+        }
+    }
+
     return (
         <div className="new-book-container">
             <div className="content">
@@ -19,11 +56,27 @@ export default function NewBook() {
                         Início
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Título"/>
-                    <input placeholder="Autor"/>
-                    <input type="date"/>
-                    <input placeholder="Preço"/>
+                <form onSubmit={ createNewBook }>
+                    <input
+                        placeholder="Título"
+                        value={title}
+                        onChange={ e => setTitle(e.target.value)}
+                    />
+                    <input
+                        placeholder="Autor"
+                        value={author}
+                        onChange={ e => setAuthor(e.target.value)}
+                    />
+                    <input
+                        type="date"
+                        value={launchDate}
+                        onChange={ e => setLaunchDate(e.target.value)}
+                    />
+                    <input
+                        placeholder="Preço"
+                        value={price}
+                        onChange={ e => setPrice(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Adicionar</button>
                 </form>
